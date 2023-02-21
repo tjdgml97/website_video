7fd05e958a1b3bf8922dc277ce29f21a364b1ff8
성희 <86584041+tjdgml97@users.noreply.github.com>
2023-02-21 14:18:26 +0900

    add background

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const snbBtn = document.querySelector("#snb .btn-menu");
+const subMenu = document.querySelector("#snb .sub-menu");
+const Winter = document.querySelector(".winter");
+const bg = document.querySelector(".bgbtn")
+
+snbBtn.addEventListener("click", function() {
+  if(subMenu.classList.contains("hide")){
+    subMenu.classList.remove("hide");
+  } else {
+    subMenu.classList.add("hide");
+  }
+})
+
+bg.addEventListener("click" ,function(){
+  if(Winter.classList.contains("hide")){
+    Winter.classList.remove("hide");
+  }else {
+    Winter.classList.add("hide");
+  }
+})
+
