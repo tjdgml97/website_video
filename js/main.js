bf9b6e25bccaf9e187e9f7ab74e81649012a8b5a
성희 <86584041+tjdgml97@users.noreply.github.com>
2023-02-21 22:30:12 +0900

    add winter video

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
 const snbBtn = document.querySelector("#snb .btn-menu");
 const subMenu = document.querySelector("#snb .sub-menu");
 const Winter = document.querySelector(".winter");
-const bg = document.querySelector(".bgbtn")
+const Summer = document.querySelector(".summer");
+const winterBtnEl = document.querySelector(".winterBtn");
+const summerBtnEl = document.querySelector(".summerBtn");
+const bgVideoEl = document.querySelector(".bgVideo");
+
+const winterbg = document.createElement("video");
+winterbg.innerHTML = '<video src="./video/snow.mp4" autoplay muted loop></video>'
 
 snbBtn.addEventListener("click", function() {
   if(subMenu.classList.contains("hide")){
@@ -11,12 +17,28 @@ snbBtn.addEventListener("click", function() {
   }
 })
 
-bg.addEventListener("click" ,function(){
-  if(Winter.classList.contains("hide")){
-    Winter.classList.remove("hide");
-    
-  }else {
-    Winter.classList.add("hide");
-  }
+winterBtnEl.addEventListener("click",function() {
+  // alert("hi");
+  bgVideoEl.innerHTML = '<video src="./video/snow.mp4" autoplay muted loop></video>';
 })
+
+summerBtnEl.addEventListener("click",function() {
+  // alert("hi");
+  bgVideoEl.innerHTML = '<video src="./video/video.mp4" autoplay muted loop></video>';
+})
+
+
+
+
+// bg.addEventListener("click" ,function(){
+//   if(Winter.classList.contains("hide")){
+//     Winter.classList.remove("hide");
+//     Summer.classList.add("hide");
+
+//   }else {
+//     Summer.classList.add("hide");
+//     Summer.classList.remove("hide");
+
+//   }
+// })
 
